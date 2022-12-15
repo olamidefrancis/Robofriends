662709384d232f064d8835d416158c475d28487c
olamidefrancis <olamidefrancis97@gmail.com>
2022-12-14 21:24:01 -0700

    Add project files

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# Robofriends
-A single page React application  showing different Robots and a search box
+# robofriends
+Tutorial for React
+To run the project:
+
+1. Clone this repo
+2. Run `npm install`
+3. Run `npm start`
---
+++ src/containers/App.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      robots: [],
+      searchfield: ''
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response=> response.json())
+      .then(users => {this.setState({ robots: users})});
+  }
+
+  onSearchChange = (event) => {
+    this.setState({ searchfield: event.target.value })
+  }
+
+  render() {
+    /*this desteeuctures our state ..this gets the value of robot we search, converts 
+    it to lower case and checks if this name matches any robot available the 
+    returns this robot name
+    */
+    const { robots, searchfield } = this.state;
+    const filteredRobots = robots.filter(robot =>{
+      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+    })
+    return !robots.length ?
+      <h1>Loading</h1> :
+      (
+        <div className='tc'>
+          <h1 className='f1'>RoboFriends</h1>
+          <SearchBox searchChange={this.onSearchChange}/>
+          <Scroll>
+            <CardList robots={filteredRobots} />
+          </Scroll>
+        </div>
+      );
+  }
+}
+
+export default App;
